4eec428a7a8ab0c01f4ae5d0a48bd8ebfdeb7098
Ritwik <dasritwik16@gmail.com>
2026-04-28 03:37:46 +0530

    fix: updated

--- app/utils/http.ts
+++ app/utils/http.ts
@@ -2,10 +2,12 @@ import axios from "axios";
 
 const authApi = axios.create({
   baseURL: process.env.NEXT_PUBLIC_AUTH_URL,
+  withCredentials: true,
 });
 
 const appApi = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
 });
 
 export { authApi, appApi };
